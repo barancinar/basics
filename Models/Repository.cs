c52595ad7af3b873a2031508aeff155fd00b291a
barancinar <cinarbaran2003@gmail.com>
2024-09-23 09:43:08 +0300

    last commit

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -8,11 +8,45 @@ namespace basics.Models
         {
             _courses = new List<Course>()
             {
-                new Course() { Id = 1, Title = "Asp.Net Kursu", Image="1.png", Description = "Güzel bir kurs gibi görünyor." },
-                new Course() { Id = 2, Title = "Python Kursu", Image="2.png", Description = "Başarılı olabilecek bir kurs olması yönünde ilerliyor." },
-                new Course() { Id = 3, Title = "Django Kursu", Image="3.png", Description = "En iyi olmaya aday gibi görünyor." },
-                new Course() { Id = 4, Title = "JavaScript Kursu", Image="4.png", Description = "Rakip kurslar arasından en iyisi olmasına rağmen geliştirilmeye ihtiyacı var." },
-                new Course() { Id = 5, Title = "C++ Kursu", Image="2.png", Description = "Rakip kurslar arasından en iyisi." }
+                new Course() { 
+                    Id = 1, 
+                    Title = "Asp.Net Kursu", 
+                    Image="1.png", 
+                    Description = "Güzel bir kurs gibi görünyor.",
+                    Tags = new string[] {"aspnet", "web geliştirme"},
+                    isActive = true,
+                    isHome = true },
+                new Course() { 
+                    Id = 2,
+                    Title = "Python Kursu", 
+                    Image="2.png", 
+                    Description = "Başarılı olabilecek bir kurs olması yönünde ilerliyor.",
+                    Tags = new string[] {"python", "temel"},
+                    isActive = true,
+                    isHome = true },
+                new Course() { 
+                    Id = 3, 
+                    Title = "Django Kursu", 
+                    Image="3.png", 
+                    Description = "En iyi olmaya aday gibi görünyor.",
+                    Tags = new string[] {"python", "temel"},
+                    isActive = false,
+                    isHome = true },
+                new Course() { 
+                    Id = 4, 
+                    Title = "JavaScript Kursu", 
+                    Image="4.png", 
+                    Description = "Rakip kurslar arasından en iyisi olmasına rağmen geliştirilmeye ihtiyacı var.",
+                    isActive = true,
+                    isHome = false },
+                new Course() { 
+                    Id = 5, 
+                    Title = "C++ Kursu", 
+                    Image="2.png", 
+                    Description = "Rakip kurslar arasından en iyisi.",
+                    Tags = new string[] {"cpp", "gömülü"},
+                    isActive = true,
+                    isHome = false }
 
 
             };
--- obj/Debug/net8.0/basics.AssemblyInfo.cs
+++ obj/Debug/net8.0/basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0074018b12210743a751e05225006e1a79278fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
